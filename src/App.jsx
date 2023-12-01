354701986a475b2f326fc11ff9db0368687d4edc
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-01 10:26:20 +0800

    initial App code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react"
+import Country from "./components/Country"
+import { useCountry, useField } from "./hooks"
 
 function App() {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = e => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
 
   return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput}/>
+        <button>find</button>
+      </form>
+
+      <Country country={country}/>
+    </div>
   )
 }
 
